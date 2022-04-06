877542cfd5b263bb841e6a4937381f3ca807da58
Pedro Aires <1212436@sga.pucminas.br>
2022-04-06 14:51:52 -0300

    testes <SectionBackground /> e <SectionContainer />

--- src/components/SectionContainer/SectionContainer.test.jsx
+++ src/components/SectionContainer/SectionContainer.test.jsx
@@ -2,9 +2,14 @@ import { screen } from "@testing-library/react";
 import { renderTheme } from "../../styles/render-theme";
 import { SectionContainer } from ".";
 
-describe("SectionContainer", () => {
+describe("SectionContainer content", () => {
   it("should render", () => {
-    renderTheme(<SectionContainer>Children</SectionContainer>);
+    const { container } = renderTheme(
+      <SectionContainer>
+        <h1>Children</h1>
+      </SectionContainer>,
+    );
     expect(screen.getByRole("heading")).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
   });
 });
