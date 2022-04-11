7066f27d5eea637cecd71ab017b6c1a23462e1d1
Pedro Aires <1212436@sga.pucminas.br>
2022-04-11 14:10:03 -0300

    <GridTwoColumns /> criado e testado

---
+++ src/components/GridTwoColumns/mock.js
@@ -0,0 +1,5 @@
+export default {
+  title: "Grid Two Columns",
+  text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure dolores magni minus maxime tempore quos?`,
+  imageSrc: "assets/images/javascript.svg",
+};
--- src/components/GridTwoColumns/stories.jsx
+++ src/components/GridTwoColumns/stories.jsx
@@ -1,15 +1,13 @@
 import { GridTwoColumns } from ".";
+import mock from "./mock";
 
 export default {
   title: "GridTwoColumns",
   component: GridTwoColumns,
-  args: {
-    title: "Grid Two Columns",
-    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure dolores magni minus maxime tempore quos?`,
-    imageSrc: "assets/images/javascript.svg",
-  },
+  args: mock,
   argTypes: {
-    children: { type: "string" },
+    text: String,
+    title: String,
   },
 };
 
